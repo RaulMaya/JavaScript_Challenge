2283afdd973a4a5f5bd5b1718a5b55dfb3abd680
Raul <raulmayas20@gmail.com>
2021-06-21 10:43:36 -0500

    Appending Table

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,11 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach((element) => {
+  var row = tbody.append("tr");
+  Object.values(element).forEach((value)=>{
+    row.append("td").text(value);
+  })
+});
