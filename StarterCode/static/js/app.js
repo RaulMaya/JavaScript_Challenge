aa3ababeba19c6606610a90fd19ab1081c9f0edc
Raul <raulmayas20@gmail.com>
2021-06-21 11:39:43 -0500

    Selecting Button and Forms and Creating Event Handlers

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,14 @@
 // from data.js
 var tableData = data;
 
+// Select the button and the form
+var button = d3.select("#button");
+var form = d3.select("#form");
+
+// Creating the event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter)
+
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
